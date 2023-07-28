eb017052e6c271b9ca46d2057311b39c1c8aa5af
eugene isingizwe <eugeneisingizwe95@hotmail.com>
2023-07-28 12:45:06 -0600

    fixed js issue

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -6,6 +6,8 @@ function getParams() {
   // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
   var searchParamsArr = document.location.search.split('&');
 
+  console.log(document.location);
+
   // Get the query and format values
   var query = searchParamsArr[0].split('=').pop();
   var format = searchParamsArr[1].split('=').pop();
